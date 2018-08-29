4ab06872228ecdbcfa4703bd482a1ec735ae597f
Dylan Alcazar <dylan.alcazar@gmail.com>
2018-08-29 16:10:53 -0600

    wip wbs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react'
+import 'babel-polyfill'
+
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+
+/* SCSS */
+import './assets/css/main.css'
+
+/* COMPONENTS */
 import Count from './components/Count'
 
+/* PAGES */
+import Home from './pages/home/'
+import Login from './pages/login/'
 
-class App extends Component {
+
+class App extends React.Component {
   render() {
     return (
-      <div>
-        <Count />
-      </div>
+      <main>
+        <Switch>
+          <Route exact path='/' component={Login} />
+          <Route exact path='/home' component={Home} />
+          <Route exact path='/counter' component={Count} />
+        </Switch>
+        <Link to={'/home'}> Home </Link>
+        <Link to={'/counter'}> Counter </Link>
+      </main>
     );
   }
 }
