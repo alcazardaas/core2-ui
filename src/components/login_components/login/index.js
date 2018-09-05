a17ccaa2513f9e7350c25622ca9e1556e6a1ca01
Dylan Alcazar <dylan.alcazar@gmail.com>
2018-09-05 08:49:05 -0600

    added redux-form to login unfuntional

--- src/components/login_components/login/index.js
+++ src/components/login_components/login/index.js
@@ -1,50 +1,45 @@
 import React from 'react'
 import logo from './../../../assets/img/2.png'
 
-import WwbInput from '../../fields/wwbInput'
-
 import { Field, reduxForm } from 'redux-form'
 
-const Login_Component = ({
-  doSubmit,
-  handleSubmit,
-  pristine,
-  reset,
-  submitting }) => (
-    <form onSubmit={handleSubmit(doSubmit)} className='add-edit-form'>
-      <fieldset>
-        <legend>Datos de la posición</legend>
-        <fieldset className='add-edit-form__radchkset'>
-          <div className='row middle-xs form-group-radcheck add-edit-form__row'>
-            <div className='col-xs-12 col-sm-3'>
-              <legend>Tipo de Empleo</legend>
-            </div>
-            <div className='col-xs-12 col-sm-9'>
-              <Field
-                name='type'
-                value='Tiempo Completo'
-                component={WwbInput}
-                type='radio'
-                id='type'
-                label='Tiempo Completo'
-              />
-              <Field
-                name='type'
-                value='Medio Tiempo'
-                component={WwbInput}
-                type='radio'
-                id='type'
-                label='Medio Tiempo'
-              />
+const Login_Component = (props) => {
+
+  const { handleSubmit, pristine, reset, submitting } = props
+
+  return (
+    <div className='offset-by-six five columns login-main-cont'>
+      <div className="login-cont">
+        <div className="logo-cont">
+          <img className='login-logo' src={logo} alt='Logo' />
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="userId">User</label>
+            <div>
+              <Field className='login-input' name='userId' component='input' type='text' placeholder='USER' />
             </div>
           </div>
-        </fieldset>
-      </fieldset>
-    </form>
+          <div>
+            <label htmlFor="passw">Password</label>
+            <div>
+              <Field className='login-input' name='passw' component='input' type='password' placeholder='USER' />
+            </div>
+          </div>
+          <div>
+            <button className='btn-login' type="submit" disabled={pristine || submitting}>Submit</button>
+            <button className='btn-login' type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
+}
 
 
-export default Login_Component
+export default reduxForm({
+  form: 'login'
+})(Login_Component)
 
 
 /**
