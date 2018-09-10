4343b1e6bceb29380c4b3853e7a29e01e68fbe10
Dylan Alcazar <dylan.alcazar@gmail.com>
2018-09-10 00:36:41 -0600

    cant login

--- src/redux/actionCreators/login.js
+++ src/redux/actionCreators/login.js
@@ -5,7 +5,7 @@ import history from './../../helpers/history'
 const API_URL_USER = 'https://localhost:44353/api/useraccounts/login'
 
 export default function login(userAccount) {
-
+  console.log(userAccount)
   return async dispatch => {
     // Initiate loading state
     dispatch({
